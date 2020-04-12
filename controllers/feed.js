0f4c475ce66759b981dc639f037072e8044bcec2
MrCookiez <theodorosvragkos@gmail.com>
2020-04-13 01:19:46 +0200

    before connecting with db

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,17 +1,58 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{ title: 'title', content: 'first post' }]
+        posts: [
+            {
+                id: '1',
+                title: 'title',
+                author: 'Theodoros Vragkos',
+                content: {
+                    text: 'first post - Lorem ipsum dolor amet',
+                    image: 'https://i.picsum.photos/id/13/200/300.jpg',
+                    links: {
+                        source: 'http://teovragkos.com'
+                    },
+                },
+            },
+            {
+                id: '2',
+                title: 'Header 2',
+                author: 'Theodoros Vragkos',
+                content: {
+                    text: 'first post - Lorem ipsum dolor amet',
+                    image: 'https://i.picsum.photos/id/13/200/300.jpg',
+                    links: {
+                        source: 'http://teovragkos.com'
+                    },
+                },
+            }
+        ]
     });
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: 'Validation failed, entered data is incorrect',
+            errors: errors.array()
+        });
+    }
+
     const title = req.body.title;
-    const content = req.body.content;
+    const text = req.body.text;
+    const author = req.body.author;
 
     // Create post in db
-   res.status(201).json({
-       message: 'Post was created!',
-       post: {
-           id: new Date().toISOString(), title: title, content: content }
-   });
+    res.status(201).json({
+        message: 'Post was created!',
+        post: {
+            id: new Date().toISOString(),
+            title: title,
+            text: text,
+            author: author
+        }
+    });
 };
